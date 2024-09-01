8c5bf46134087e20df1df123ac18945e24517867
sahebmanna <110231937+sahebmanna@users.noreply.github.com>
2024-09-01 20:46:37 +0000

    string compleate

---
+++ 01_basics/string.js
@@ -0,0 +1,37 @@
+let fname=' saheb '
+let sname=' manna'
+console.log(fname + sname);
+
+//using backtix(`...`) ,string interpolation,using placeholder
+console.log(`my first Name is ${fname} and surname is ${sname}`);
+
+//new key word is used to access JS objecte
+
+const gamename=new String("  bgmifight ")
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+//const gamenameSplit=gamename.length();
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf('m'));
+console.log(gamename.substring(0,4));
+console.log(gamename);
+
+console.log(gamename.trim());
+const url="http://saheb.com/saheb_manna"
+console.log(url.length);
+
+console.log(url.replace("%4","_"));
+const newur=url.slice(0,5);
+console.log(newur);
+
+
+
+
+
+
+
+
+
