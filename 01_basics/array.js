cfc004b1b131845078b6aca6cafaa9a21aab55af
sahebmanna <110231937+sahebmanna@users.noreply.github.com>
2024-09-08 19:26:53 +0000

    array compleate

---
+++ 01_basics/array.js
@@ -0,0 +1,43 @@
+let myarray=[1,2,3,4,5]//first type of array declertation
+//console.log(myarray);
+
+let myarray2=new Array(3,4,5,6)
+console.log(myarray2);    //second types of array decleration
+myarray2.push(9)
+console.log(myarray2);
+
+myarray2.unshift(2)
+console.log(myarray2);
+
+myarray2.shift()
+console.log(myarray2);
+myarray2.shift();
+console.log(myarray2);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+ console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+
+
+
