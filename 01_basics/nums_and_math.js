078d7486dc7f962afe6bb6165b16a44e7bf773ca
sahebmanna <110231937+sahebmanna@users.noreply.github.com>
2024-09-08 15:02:09 +0000

    mathematics

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,23 @@
+const score=100
+console.log(score);
+
+const balance=new Number(100.1)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed());
+
+const othernumber=new Number(123.897)
+console.log(othernumber.toPrecision(4));
+
+const xyz=1000000
+console.log(xyz.toLocaleString());
+
+console.log(math);
+
+
+
+
+
+
+
+
