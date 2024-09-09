b9b2399beb62368686279c49d8f9c61eaaca7571
sahebmanna <110231937+sahebmanna@users.noreply.github.com>
2024-09-09 11:15:52 +0000

    array operations

---
+++ 02_basics/array_2.js
@@ -0,0 +1,46 @@
+ let myarray=[1,2,3,4,5]//first type of array declertation
+// //console.log(myarray);
+
+// let myarray2=new Array(3,4,5,6)
+// console.log(myarray2);    //second types of array decleration
+// myarray2.push(9)
+// console.log(myarray2);
+
+// myarray2.unshift(2)
+// console.log(myarray2);
+
+// myarray2.shift()
+// console.log(myarray2);
+// myarray2.shift();
+// console.log(myarray2);
+
+let myArr=[1,2,3,4,5]
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// // slice, splice
+
+ console.log("A ", myArr);
+
+const myn1 = myArr.slice(0, 3)//1 is starting index 
+                              //and 3 is size
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(0, 3)//index 2 se 2+3 tak print hoga
+console.log(myn2);
+console.log("C ", myArr);
+
+
+
+
+
+
